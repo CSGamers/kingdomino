3d5582e3ad32de21f48169dcffe03ce2ba9a71d4
Kyle Combs <combskyle@gmail.com>
2018-12-12 16:13:14 -0500

    added kings

--- src/client/components/containers/App.js
+++ src/client/components/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Board from "../display/Board";
-import Piece from "../display/Piece";
+import King from "../display/King";
 import { connect } from "react-redux";
 import * as actions from "../../actions/actions";
 import CurrContainer from "../containers/CurrContainer";
@@ -8,7 +8,7 @@ import NextContainer from "../containers/NextContainer";
 import NextPieceContainer from "./NextPieceContainer";
 
 const mapStateToProps = store => ({
-  boards: store.game.boards
+  boards: store.game.boards,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -38,13 +38,25 @@ class App extends Component {
     console.log('boards', this.props.boards);
     return (
       <div id="app">
-        <Board id={1} contents={this.props.boards.board1}/>
+        <div id="board1Container">
+          <Board id='board1' contents={this.props.boards.board1}/>
+          <div className="kingContainer">
+            <King color="red" />
+            <King color="red" />
+          </div>
+        </div>
         <div className="controls">
           <CurrContainer />
           <NextContainer />
           <NextPieceContainer />
         </div>
-        <Board id={2} contents={this.props.boards.board2} />
+        <div id="board2Container">
+          <Board id='board2' contents={this.props.boards.board2}/>
+          <div className="kingContainer">
+            <King color="blue" />
+            <King color="blue" />
+          </div>
+        </div>
 
       </div>
     );
